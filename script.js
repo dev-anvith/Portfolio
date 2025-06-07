2af59686c3976357e2a18cbebff6b60d28909a87
dev-anvith <anvithrh@gmail.com>
2025-06-07 22:33:40 +0530

    Final Touch

--- script.js
+++ script.js
@@ -2,37 +2,41 @@ const projects = [
     {
         title: "TrustNet",
         description: "TrustNet is a backend project that provides real-time social proofing services, such as displaying live viewers and recent purchase records. It helps businesses build credibility and boost conversions by showcasing user activity transparently.",
-        // backgroundImage: "url('https://picsum.photos/200')"
+        url:"https://github.com/dev-anvith/TrustNet"
     },
     {
         title: "Multi KeyWord Spotting",
         description: "Multi-KWS Voice Activation Model with RoPE Transformers achieves 99.3% SOTA accuracy. It delivers precise, low-latency keyword spotting for multiple wake words, perfect for voice-controlled and edge devices.",
-        // backgroundImage: "url('https://picsum.photos/200')"
+        url:"#"
     },
     {
         title: "Scatch",
         description: "Scatch is a backend-focused e-commerce platform for designer bags. It manages user authentication, orders, inventory, and payments securely. The system provides fast, reliable APIs to power a smooth shopping experience.",
-        // backgroundImage: "url('https://picsum.photos/200')"
+         url:"https://github.com/dev-anvith/scatch"
     }
 ];
 
 const projectsContainer = document.getElementById('projects-container');
 
-function addProject(title, description, backgroundImage) {
+function addProject(title, description, url) {
     const projectBlock = document.createElement('div');
     projectBlock.classList.add('project-block');
     projectBlock.setAttribute('style', 'height: fit-content;');
 
     // projectBlock.style.backgroundImage = backgroundImage;
 
-    projectBlock.innerHTML = `
-        <h3>${title}</h3>
-        <p>${description}</p>
-        <button>Check it out</button>
-    `;
+ projectBlock.innerHTML = `
+  <h3>${title}</h3>
+  <p>${description}</p>
+  <button class="read-more-btn">
+    <a href="${url}" >Check it out</a>
+  </button>
+`;
+
+
 
     projectsContainer.appendChild(projectBlock);
 }
 
 // Add initial projects with background images
-projects.forEach(project => addProject(project.title, project.description, project.backgroundImage));
+projects.forEach(project => addProject(project.title, project.description, project.url));
